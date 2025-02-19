1cd9296fed653ac4009667d85a772087e97fd6c8
Husain Alshaikhahmed <59243432+HusainCode@users.noreply.github.com>
2025-02-19 05:24:07 -0600

    Update working_with_strings.py

--- Coding-Exercise/Strings/working_with_strings.py
+++ Coding-Exercise/Strings/working_with_strings.py
@@ -12,3 +12,11 @@
 # If there is more than one word with the maximum number of characters, return the first such word that exists in the string.
 
 # Example: For input Once I'm awaken, I'll sacrifice your soul to the ruler of darkness. the output should be sacrifice.
+
+############################### Sample Story ###############################
+# sample_story = '''Once upon a time, there was a beginner programmer named Alice who was eager to learn Python.
+#She tried to learn from books, but found it difficult to grasp the concepts. One day, she stumbled upon an online course.
+
+# Alice was thrilled. The course was taught by a well-known programmer who made the lessons interesting and easy to understand.
+# The course covered everything a beginner programmer needed, and Alice was finally able to understand how to code in Python.'''
+############################### End Of Story ###############################
