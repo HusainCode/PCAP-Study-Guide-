5a5b913711dd6926beaceb55ecdf618615f98566
Husain Alshaikhahmed <59243432+HusainCode@users.noreply.github.com>
2025-02-26 04:19:48 -0600

    Update human.py

--- Projects/Rock-paper-scissors/human.py
+++ Projects/Rock-paper-scissors/human.py
@@ -3,8 +3,21 @@ from player import Player
 
 
 class Human(Player, ABC):
-    def __init__(self, score):
-        super().__init__(score)
 
-        def make_choice(self):
-            user_selection_input = input("Rock, paper or scissors [r/p/s]?")
+    _instance = None
+
+    def __init__(self,score = 0):
+        super().__init__()
+        self.score = score
+
+        def __init__():
+            # Only one object is allowed since only one Human is running at a time.
+            def __new__(cls):
+                if cls._instance is None:
+                    cls._instance = super().__new__(cls)
+                return cls._instance
+
+    def make_choice(self):
+        user_selection_input = input("Rock, paper or scissors [r/p/s]?")
+        return user_selection_input
+
