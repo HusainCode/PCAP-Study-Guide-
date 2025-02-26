e1db9ab3f92ef4eeeead6c491d66bb9ed4ecfdd2
Husain Alshaikhahmed <59243432+HusainCode@users.noreply.github.com>
2025-02-26 04:19:00 -0600

    Update game.py

--- Projects/Rock-paper-scissors/game.py
+++ Projects/Rock-paper-scissors/game.py
@@ -1,46 +1,59 @@
 from human import Human
 from computer import Computer
-
+from types import MappingProxyType # Prevents accidental modifications
 
 class Game:
+
+    human = Human()
+    computer = Computer()
+
+    winner = None # holds the winner status
+
+    # Each key beats the value assigned to it
+    rules = MappingProxyType({"r": "s",  # rock beats scissors
+             "s": "p",  # scissors beats paper
+             "p": "r"})  # # paper beats rock
+
     _instance = None
 
     def __inti__(self):
-
-        self.result = None
-
-        def __new__(cls, *args, **kwargs):
+        # Only one object is allowed since there is only one game running at a time.
+        def __new__(cls):
             if cls._instance is None:
                 cls._instance = super().__new__(cls)
             return cls._instance
 
-    def determine_winner(self, number_rounds,human_selection,computer_selection):
+    # this function determine the winner of the game
+    def determine_winner(self,human_selection,computer_selection):
 
-        count = 0
-        while number_rounds > count:
-            if human_selection == 'r' and self.computer.make_selection == 'p':
-                self.human.score += 1
-            elif human_selection == 'r' and self.computer.make_selection == 's':
-                self.computer.score += 1
-            elif human_selection == 'p' and self.computer.make_selection == 'r':
-                self.computer.score += 1
-            elif human_selection == 'p' and self.computer.make_selection == 's':
-                self.computer.score += 1
-            elif human_selection == 's' and self.computer.make_selection == 'r':
-                self.computer.score += 1
-            elif human_selection == 's' and self.computer.make_selection == 'p':
-                self.computer.score += 1
-            count += 1
-
-        if self.computer.score > self.computer.score:
-            print("You won!")
-        elif self.computer.score < self.computer.score:
-            print("Computer won!")
+        if Game.rules[human_selection] == computer_selection: # example: r VS p
+            Game.human.score +=1 # give human a point
+        elif human_selection == computer_selection:
+            pass
         else:
-            print("It was a tie")
+            Game.computer.score += 1 # give computer a point
 
+        # holds the winner of the game according the scores
+        Game.winner = ("You won" if Game.human.score > Game.computer.score
+                       else "Tie Game" if Game.human.score == Game.computer.score
+                       else "You lost")
+
+    # Display summer for the rounds
+    def round_summry(self,human_selection,computer_selection):
+        print(f"You:{human_selection} | Computer:{computer_selection}")
+
+        if Game.rules[human_selection] == computer_selection:
+            print("You won!")
+        elif human_selection == computer_selection:
+            print("It was a tie!")
+        else:
+            print("Computer won.")
+
+    # Display the final results of the game. Who Won!
     def display_results(self):
-        print(f" you {self.human.score} | Computer {self.computer.score}")
+        print(f"\n[Game Summary] Your points {Game.human.score} | Computer points {Game.computer.score}")
+        print(Game.winner)
+
 
 
 if __name__ == "__main__":
@@ -49,11 +62,14 @@ if __name__ == "__main__":
     number_rounds_input = int(input("How many rounds would you like to play? "))
 
     game = Game()
-    human = Human()
-    computer = Computer()
 
-    human.make_choice(number_rounds_input)
-    computer.make_choice()
+    for i in range(number_rounds_input):
+        human_choice  = game.human.make_choice()
+        computer_choice = game.computer.make_choice()
 
-    game.determine_winner(number_rounds_input)
+        game.determine_winner(human_choice,computer_choice)
+        game.round_summry(human_choice,computer_choice)
+
     game.display_results()
+
+
