3c9d981df0383e6a9a272ecf412ff8cea3694a48
Husain Alshaikhahmed <59243432+HusainCode@users.noreply.github.com>
2025-02-26 04:17:37 -0600

    Update computer.py

--- Projects/Rock-paper-scissors/computer.py
+++ Projects/Rock-paper-scissors/computer.py
@@ -4,8 +4,20 @@ import random
 
 
 class Computer(Player, ABC):
-    def __inti__(self, score):
-        super().__init__(score)
+    instance = None
 
-    def make_selection(self):
-        pass
+    def __init__(self, score=0):
+        super().__init__(score)
+        self.selection = 'rps' # selection options
+
+    def __init__():
+        # Only one object is allowed since only one Human is running at a time
+        def __new__(cls):
+            if cls._instance is None:
+                cls._instance = super().__new__(cls)
+            return cls._instance
+
+    # makes random Computer selection
+    def make_choice(self):
+        random_selection = random.choice(self.selection)
+        return random_selection
